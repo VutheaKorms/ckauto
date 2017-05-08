f0c44f232682481857c5ce0bd275ee53602f8449
So Thea <vutheakorm168@gmail.com>
2017-05-08 16:46:30 +0700

    modified styles and login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,91 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+    <div class="app flex-row align-items-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-5">
+                    <div class="card-group mb-0">
+                        <div class="card p-2">
+                            <div class="card-block">
+                                <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+                                    {{ csrf_field() }}
+                                    <h1>Login</h1>
+                                    <p class="text-muted">Sign In to your account</p>
+                                    <div class="input-group mb-1">
+                                    <span class="input-group-addon"><i class="icon-user"></i>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
+                                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                            {{--<div class="col-md-6">--}}
+                                                <input id="email" type="email" class="form-control" name="email" placeholder="Email" value="{{ old('email') }}" required autofocus>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
+                                                @if ($errors->has('email'))
+                                                    <span class="help-block">
+                                                        <strong>{{ $errors->first('email') }}</strong>
+                                                    </span>
+                                                @endif
+                                            {{--</div>--}}
+                                        </div>
+                                    </div>
+                                    <div class="input-group mb-2">
+                                    <span class="input-group-addon"><i class="icon-lock"></i>
                                     </span>
-                                @endif
+                                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+
+                                            {{--<div class="col-md-6">--}}
+                                                <input id="password" type="password" class="form-control" placeholder="Password" name="password" required>
+
+                                                @if ($errors->has('password'))
+                                                    <span class="help-block">
+                                                        <strong>{{ $errors->first('password') }}</strong>
+                                                    </span>
+                                                @endif
+                                            {{--</div>--}}
+                                        </div>
+                                    </div>
+                                    </span>
+                                    <div class="form-group">
+                                        <div>
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <button type="submit" class="btn btn-primary px-2">Login</button>
+                                        </div>
+                                        <div class="col-6 text-right">
+                                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                Forgot Your Password?
+                                            </a>
+                                            {{--<button type="button" class="btn btn-link px-0">Forgot password?</button>--}}
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
                         </div>
+                        <div style="width:14%">
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                        <!--<div class="card card-inverse card-primary py-3 hidden-md-down" style="width:44%">-->
+                        <!--<div class="card-block text-center">-->
+                        <!--<div>-->
+                        <!--<h2>Sign up</h2>-->
+                        <!--<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>-->
+                        <!--<button type="button" class="btn btn-primary active mt-1">Register Now!</button>-->
+                        <!--</div>-->
+                        <!--</div>-->
+                        <!--</div>-->
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,48 @@
 <!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
+<html lang="{{ config('app.locale') }}" ng-app="app" class="ng-scope">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style type="text/css">@charset "UTF-8";[ng\:cloak],[ng-cloak],[data-ng-cloak],[x-ng-cloak],.ng-cloak,.x-ng-cloak,.ng-hide:not(.ng-hide-animate){display:none !important;}ng\:form{display:block;}.ng-animate-shim{visibility:hidden;}.ng-anchor{position:absolute;}</style>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="CoreUI - Free Bootstrap 4 Admin Template">
+    <meta name="keyword" content="bootstrap, template, admin, angular, jquery">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    {{--<title>{{ config('app.name', 'Laravel') }}</title>--}}
+    <title>{{ config('app.name', 'CkAuto') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{--<link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
 
+    <script src="bower_components/jquery/jquery.min.js"></script>
+    <script src="bower_components/angular/angular.min.js"></script>
+
+    <script src="bower_components/angular-ui-router/release/angular-ui-router.min.js"></script>
+    <script src="bower_components/oclazyload/dist/ocLazyLoad.min.js"></script>
+    {{--<script src="bower_components/jquery/dist/jquery.min.js"></script>--}}
+    <script src="bower_components/angular-breadcrumb/dist/angular-breadcrumb.min.js"></script>
+    <script src="bower_components/angular-loading-bar/build/loading-bar.min.js"></script>
+
+    <!-- Main styles for this application -->
+    <link href="app/css/style.css" rel="stylesheet">
+    <link href="app/css/styles.css" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="app/css/font-awesome.min.css">
+    <link type="text/css" rel="stylesheet" href="app/css/simple-line-icons.css">
+
+    <script src="app/js/app.js"></script>
+    {{--<script src="app/js/libs/app.js"></script>--}}
+    <script src="app/js/routes.js"></script>
+
+    <script src="app/js/demo/routes.js"></script>
+
+    <script src="app/js/controllers.js"></script>
+    <script src="app/js/directives.js"></script>
+
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     <!-- Scripts -->
     <script>
         window.Laravel = {!! json_encode([
@@ -20,68 +50,93 @@
         ]) !!};
     </script>
 </head>
+
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
+    @if (Auth::guest())
         @yield('content')
+        {{--<li><a href="{{ route('login') }}">Login</a></li>--}}
+        {{--<li><a href="{{ route('register') }}">Register</a></li>--}}
+    @else
+    <header class="app-header navbar">
+    <button class="navbar-toggler mobile-sidebar-toggler hidden-lg-up" type="button">&#9776;</button>
+    <a class="navbar-brand" ui-sref="app.main"></a>
+    <ul class="nav navbar-nav hidden-md-down">
+    <li class="nav-item">
+    <a class="nav-link navbar-toggler sidebar-toggler" href="#">&#9776;</a>
+    </li>
+
+    <li class="nav-item px-1">
+    <a class="nav-link" href="#">Dashboard</a>
+    </li>
+    <li class="nav-item px-1">
+    <a class="nav-link" href="#">Users</a>
+    </li>
+    <li class="nav-item px-1">
+    <a class="nav-link" href="#">Settings</a>
+    </li>
+    </ul>
+    <ul class="nav navbar-nav ml-auto hidden-md-down">
+    <li class="nav-item">
+    <a class="nav-link" href="#"><i class="icon-bell"></i><span class="badge badge-pill badge-danger">5</span></a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="#"><i class="icon-list"></i></a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="#"><i class="icon-location-pin"></i></a>
+    </li>
+    <li class="nav-item dropdown">
+
+
+    <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+    <!--<img src="img/avatars/6.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">-->
+    <span class="hidden-md-down">{{ Auth::user()->name }}</span>
+    </a>
+
+    <ul class="dropdown-menu dropdown-menu-right">
+    <div class="dropdown-header text-center">
+    <strong>Account</strong>
     </div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <a class="dropdown-item" href="#"><i class="fa fa-bell-o"></i> Updates<span class="badge badge-info">42</span></a>
+    <a class="dropdown-item" href="#"><i class="fa fa-envelope-o"></i> Messages<span class="badge badge-success">42</span></a>
+    <!--<a class="dropdown-item" href="#"><i class="fa fa-tasks"></i> Tasks<span class="badge badge-danger">42</span></a>-->
+    <!--<a class="dropdown-item" href="#"><i class="fa fa-comments"></i> Comment<span class="badge badge-warning">42</span></a>-->
+
+    <div class="dropdown-header text-center">
+    <strong>Settings</strong>
+    </div>
+
+    <a class="dropdown-item" href="#"><i class="fa fa-user"></i> Profile</a>
+    <a class="dropdown-item" href="#"><i class="fa fa-wrench"></i> Setting</a>
+    <!--<a class="dropdown-item" href="#"><i class="fa fa-usd"></i> Payments<span class="badge badge-default">42</span></a>-->
+    <!--<a class="dropdown-item" href="#"><i class="fa fa-file"></i> Projects<span class="badge badge-primary">42</span></a>-->
+    <!--<div class="divider"></div>-->
+    <!--<a class="dropdown-item" href="#"><i class="fa fa-shield"></i> Lock account</a>-->
+
+    <a class="dropdown-item" href="{{ route('logout') }}"
+    onclick="event.preventDefault();
+    document.getElementById('logout-form').submit();">
+    <i class="fa fa-lock"></i>Logout
+    </a>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+    {{ csrf_field() }}
+    </form>
+    </ul>
+
+    </li>
+
+    </li>
+
+    <li class="nav-item">
+    <a class="nav-link navbar-toggler aside-menu-toggler" href="#">&#9776;</a>
+    </li>
+    </ul>
+    </header>
+    @endif
+    {{--<!-- Scripts -->--}}
+    {{--<script src="{{ asset('js/app.js') }}"></script>--}}
 </body>
 </html>
